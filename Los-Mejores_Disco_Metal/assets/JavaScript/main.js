fc59a3f4eba66efafbf5636c6e9604676f07aa85
OscarRoy <37048172+OscarRoy@users.noreply.github.com>
2018-04-08 15:28:17 -0600

    Se subiran proyecto de Tecno-Soluciones Roy y Los mejores discos Metal

---
+++ Los-Mejores_Disco_Metal/assets/JavaScript/main.js
@@ -0,0 +1,27 @@
+let button = document.querySelectorAll("button");
+
+//recorrerlos
+
+button.forEach(function(button){
+
+  //agregar evento
+
+  button.addEventListener('click', function(ev){
+    ev.preventDefault();
+    let content = document.querySelector('.content');
+
+    //quitar clase de animacion
+    content.classList.remove("animated");
+    content.classList.remove("fadeInDown");
+
+    //agregar clase de animacion
+    content.classList.add("animated");
+    content.classList.add("fadeOutUp");
+
+    setTimeout(function(){
+      location.href = "/Los-Mejores_Disco_Metal";
+    },600);
+
+    return false;
+  })
+});
